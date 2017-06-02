aa672215b74ccf8b8acc19af5e2ce394db65ad81
vagrant <vivien.fan.1224@gmail.com>
2017-06-02 17:50:01 +0000

    add in background, spaceship, bullet and their animation

--- src/app.js
+++ src/app.js
@@ -2,21 +2,77 @@ require ('../home.scss');
 
 console.log('hello');
 
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
+const game = new Phaser.Game(800, 500, Phaser.AUTO);
+
+var spacefield, spaceship, bullets;
+var fireButton;
+var cursors;
+var bgSpeed;
+var bulletTime = 0;
 
 const gameState = {
   preload: () => {
-    console.log("preload");
+    game.load.image('spacefield', 'assets/img/spacefield.jpg');
+    game.load.image('spaceship', 'assets/img/spaceship.png');
+    game.load.image('bullet', 'assets/img/bullet.png');
   },
 
   create: () => {
-    console.log("create");
+    spacefield = game.add.tileSprite(0, 0, 800, 500, 'spacefield');
+
+    spaceship = game.add.sprite(game.world.centerX, 400, 'spaceship');
+    spaceship.scale.setTo(0.2);
+    spaceship.anchor.setTo(0.5, 0);
+    game.physics.enable(spaceship, Phaser.Physics.ARCADE);
+
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(30, 'bullet');
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 1);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    bgSpeed = 3;
   },
 
   update: () => {
-    // console.log("update");
+    spacefield.tilePosition.y += bgSpeed;
+    spaceship.body.velocity.x = 0;
+
+    if (cursors.left.isDown) {
+      spaceship.body.velocity.x = -350;
+    }
+    if (cursors.right.isDown) {
+      spaceship.body.velocity.x = 350;
+    }
+
+    if(fireButton.isDown) {
+      fireBullet();
+    }
   }
+
 };
+
+const fireBullet = () => {
+  let bullet;
+  if (game.time.now > bulletTime) {
+    console.log(bullets);
+    bullet = bullets.getFirstExists(false);
+    if (bullet) {
+      console.log(bullet);
+      bullet.anchor.setTo(0.5);
+      bullet.scale.setTo(0.02);
+      bullet.reset(spaceship.x, spaceship.y);
+      bullet.body.velocity.y -= 400;
+      bulletTime = game.time.now + 200;
+    }
+  }
+}
 
 game.state.add('gameState', gameState);
 game.state.start('gameState');
