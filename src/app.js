4103d5d3e5a9ca45db9add869e15f950b813225c
vagrant <vivien.fan.1224@gmail.com>
2017-06-02 16:22:56 +0000

    phaser init setup

--- src/app.js
+++ src/app.js
@@ -1,2 +1,22 @@
 require ('../home.scss');
+
 console.log('hello');
+
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
+
+const gameState = {
+  preload: () => {
+    console.log("preload");
+  },
+
+  create: () => {
+    console.log("create");
+  },
+
+  update: () => {
+    // console.log("update");
+  }
+};
+
+game.state.add('gameState', gameState);
+game.state.start('gameState');
