e2bfba535b4966e7a9e24b7a8a76a429e8ad3a0c
Yinghui (Vivien) Fan <vivien.fan.1224@gmail.com>
2017-06-09 20:47:22 -0400

    Create index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+process.env.PWD = process.cwd();
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(process.env.PWD+ + '/src'));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(process.env.PWD+'/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
