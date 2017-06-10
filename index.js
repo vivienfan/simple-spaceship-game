c7fa174328818647c78b79912125728f6310ef68
Yinghui (Vivien) Fan <vivien.fan.1224@gmail.com>
2017-06-09 20:50:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 app.use(express.static(__dirname + '/src'));
 app.get('/', function(req, res) {
-    res.sendFile(path.join('/index.html'));
+    res.sendFile(path.join('./index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
